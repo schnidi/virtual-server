f7669b38e0d0b8b504c41cf31054f386605eb133
schnidi <viliam.schnidi@inmail.sk>
2023-10-23 11:13:52 +0200

    Update Add_vbox_HDD.sh

--- Add_vbox_HDD.sh
+++ Add_vbox_HDD.sh
@@ -6,7 +6,7 @@ sleep 5
 	lsblk
 		read -p 'Add HDD (sdx): ' HDD1
 		read -p 'Add HDD (sdx): ' HDD2
-			VboxManage internalcommands createrawvmdk -filename /home/vbox/virtual_HDD/HDD1.vmdk  -rawdisk /dev/$HDD1
-			VboxManage internalcommands createrawvmdk -filename /home/vbox/virtual_HDD/HDD2.vmdk  -rawdisk /dev/$HDD2
+			VBoxManage internalcommands createrawvmdk -filename "/home/vbox/virtual_HDD/HDD1.vmdk"  --rawdisk /dev/$HDD1
+			VBoxManage internalcommands createrawvmdk -filename "/home/vbox/virtual_HDD/HDD2.vmdk"  --rawdisk /dev/$HDD2
 exit
 
